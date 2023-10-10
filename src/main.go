08681aad0d3778d2c549ed882126a4cf94b25365
illenium <illenium040@mail.ru>
2023-10-10 22:59:00 +0300

    Remove some code from main

--- src/main.go
+++ src/main.go
@@ -53,52 +53,6 @@ func run() error {
 		return fmt.Errorf("db ping: %w", err)
 	}
 
-	//repo := repository.NewFileRepository(con)
-
-	//path := "C:\\Users\\serj-\\Рабочий стол\\Main\\Аниме\\_--5SsruWa0.jpg"
-	//file, err := os.OpenFile(path, os.O_RDONLY, 0400)
-	//if err != nil {
-	//	return fmt.Errorf("open file: %w", err)
-	//}
-
-	//stat, err := file.Stat()
-	//if err != nil {
-	//	return fmt.Errorf("get file stat: %w", err)
-	//}
-
-	//fileMeta, _ := domain.NewFileMeta(0, 0, stat.Name(), filepath.Ext(path), path, uint32(stat.Size()), stat.ModTime(), nil)
-
-	//id, err := repo.AddFile(ctx, fileMeta, file)
-	//if err != nil {
-	//	return fmt.Errorf("add file: %w", err)
-	//}
-	//
-	//fmt.Printf("id: %d", id)
-
-	//err = repo.ReadFile(ctx, 2, func(meta domain.FileMeta, loReader io.Reader) (err error) {
-	//	file, err := os.Create(filepath.Join("./bin", meta.Name()))
-	//	if err != nil {
-	//		return fmt.Errorf("creating file: %w", err)
-	//	}
-	//
-	//	defer func() {
-	//		closeErr := file.Close()
-	//		if closeErr != nil {
-	//			err = fmt.Errorf("file closing: %v, original err: %w", closeErr, err)
-	//		}
-	//	}()
-	//
-	//	_, err = io.Copy(file, loReader)
-	//	if err != nil {
-	//		return fmt.Errorf("copy large object data to file: %w", err)
-	//	}
-	//
-	//	return nil
-	//})
-	//if err != nil {
-	//	return err
-	//}
-
 	<-stopped
 
 	return nil
